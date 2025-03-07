21829ae430a0bec4d1bdf0e78d4dfab7389c4f5d
Adam Laycock <adam@alaycock.co.uk>
2025-03-07 09:27:02 +0000

    feat: change player for filetype

--- src/utils/play.ts
+++ src/utils/play.ts
@@ -1,6 +1,17 @@
 import path from 'path'
 import player from 'play-sound'
 
+const getAudioPlayer = (extname: string) => {
+  switch (extname) {
+    case '.wav':
+      return 'aplay'
+    default:
+      return 'mpg321'
+  }
+}
+
 export const playSound = (fileName: string) => {
-  player().play(path.join(process.cwd(), 'sounds', fileName))
+  player({player: getAudioPlayer(path.extname(fileName))}).play(
+    path.join(process.cwd(), 'sounds', fileName)
+  )
 }
