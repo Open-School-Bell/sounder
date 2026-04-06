1ee4a568c10f2069402c0556b6d2c337fcbdd2c1
Adam Laycock <adam@alaycock.co.uk>
2026-04-06 16:22:12 +0100

    feat: docker install script

---
+++ install-docker.sh
@@ -0,0 +1,46 @@
+sudo apt update
+sudo apt install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
+Types: deb
+URIs: https://download.docker.com/linux/debian
+Suites: $(. /etc/os-release && echo "$VERSION_CODENAME")
+Components: stable
+Architectures: $(dpkg --print-architecture)
+Signed-By: /etc/apt/keyrings/docker.asc
+EOF
+
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin curl
+
+sudo groupadd docker
+sudo usermod -aG docker $USER
+
+sudo mkdir -p /var/osb/sounder-docker
+
+chown $USER:docker /var/osb/sounder-docker
+
+curl -fsSL https://raw.githubusercontent.com/Open-School-Bell/sounder/support/docker-compose.yml -o docker-compose.yml
+curl -fsSL https://raw.githubusercontent.com/Open-School-Bell/sounder/support/docker-bin.sh -o docker-bin.sh
+
+sudo cp ./docker-bin.sh /bin/sounder
+sudo chmod +x /bin/sounder
+
+function configure {
+  read -p "Controller Address (http://192.168.1.1:3000) " ca
+  read -p "Sounder Key " sk
+  sed -i "s#<CONTROLLER_ADDRESS>#$ca#" ./docker-compose.yml
+  sed -i "s/<SOUNDER_KEY>/$sk/" ./docker-compose.yml
+
+  docker compose pull
+  docker compose up -d
+}
+
+read -p "Do you want to configure the sounder now? [Y|n] " yn
+case $yn in
+  [Yy]* ) configure; break;;
+  [Nn]* ) exit;;
+  * ) exit;;
+esac
